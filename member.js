066ec384d6702404b142dd2cc13bc33461d088fc
siddhi-s <46420474+siddhi-s@users.noreply.github.com>
2024-10-28 18:24:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateURL: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindtoController: true,
+        scope: {
+            member: 'm'
+        }
+    };
+}
